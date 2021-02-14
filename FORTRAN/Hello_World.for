6e2588093ee98d554facfca7f74c687407e11517
Harold Toomey <harold.toomey@utc.com>
2021-02-14 16:39:47 -0600

    Add files via upload

---
+++ FORTRAN/Hello_World.for
@@ -0,0 +1,9 @@
+C NAME: Harold Toomey
+C TASK: Hello World
+C LANGUAGE:  FORTRAN 77
+C COMPILER: Microsoft FORTRAN 77 Version 3.3
+C
+
+PROGRAM HELLO_WORLD
+   print *, 'Hello, World!'
+END PROGRAM HELLO_WORLD
