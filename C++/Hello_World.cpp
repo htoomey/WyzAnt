8a015bbf6b4750057de2890534b0034295099c51
Harold Toomey <harold.toomey@utc.com>
2021-02-14 15:31:49 -0600

    Add files via upload

---
+++ C++/Hello_World.cpp
@@ -0,0 +1,14 @@
+/*
+NAME: Harold Toomey
+TASK: Hello World
+LANG: C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Hello, World!" << endl;
+    return (0);
+}
